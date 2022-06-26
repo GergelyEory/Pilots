a4f875c8c29765a229c9d8037c3a0eb8f93aa75b
gergely.eory <gergely.eory@solirius.com>
2022-06-26 20:28:17 +0100

    module declarations swapped

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { PilotListComponent } from './pilots/components/pilot-list.component';
-import { AddPilotComponent } from './pilots/components/add-pilot.component';
-import { PilotCardComponent } from './pilots/components/pilot-card.component';
 import { PilotStore } from './pilots/pilots.store';
+import { PilotsModule } from './pilots/pilots.module';
 
 @NgModule({
-	declarations: [AppComponent, PilotListComponent, PilotCardComponent, AddPilotComponent],
-	imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, PilotsModule],
 	providers: [PilotStore],
 	bootstrap: [AppComponent],
 })
--- src/app/pilots/pilots.module.ts
+++ src/app/pilots/pilots.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PilotListComponent } from './components/pilot-list.component';
+import { PilotCardComponent } from './components/pilot-card.component';
+import { AddPilotComponent } from './components/add-pilot.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-	declarations: [],
-	imports: [CommonModule],
+	declarations: [PilotListComponent, PilotCardComponent, AddPilotComponent],
+	imports: [CommonModule, AppRoutingModule, ReactiveFormsModule],
 })
 export class PilotsModule {}
